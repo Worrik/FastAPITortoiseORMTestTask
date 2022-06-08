88e4f8b92d800798b03c4e5955b93e0d4ec6edce
Worrik <worrik@vivobook.localdomain>
2022-06-08 11:28:20 +0300

    added analitics filter

--- app/routers/analitics.py
+++ app/routers/analitics.py
@@ -1,3 +1,5 @@
+import datetime
+from typing import Optional
 from fastapi import APIRouter
 from tortoise.functions import Count
 
@@ -8,10 +10,19 @@ router = APIRouter(prefix="/analitics")
 
 
 @router.get("/")
-async def get_analitics():
+async def get_analitics(
+        date_from: Optional[datetime.date] = None,
+        date_to: Optional[datetime.date] = None
+):
     query = (Like.all()
                  .annotate(date=Date('created_at'),
                            count_likes=Count('id'))
                  .group_by('date'))
+    if date_from:
+        query = query.filter(date__gte=date_from)
+
+    if date_to:
+        query = query.filter(date__lte=date_to)
+
     return await query.values('date', 'count_likes')
 
