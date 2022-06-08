a78a185369160c8643c8aa1e548e79f996639eca
Worrik <worrik@vivobook.localdomain>
2022-06-08 10:56:22 +0300

    fix

--- app/services/login.py
+++ app/services/login.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from fastapi.param_functions import Header
 from fastapi_login import LoginManager
 from app.config.base import SECRET
 from app.models.user import User
@@ -15,5 +14,6 @@ manager = LoginManager(
 async def load_user(user_id: int) -> User:
     user = await User.get(id=user_id)
     user.last_active = datetime.now()
+    await user.save()
     return user
 
